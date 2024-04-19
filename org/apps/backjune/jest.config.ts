33cd1e2004016d08b12be481fa7d8c25de3ca146
Justal Kevin <justal.kevin@gmail.com>
2024-04-19 17:40:03 +0200

    :sparkle: Testing the api june on backend

---
+++ org/apps/backjune/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'backjune',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/backjune',
+};
---
+++ org/apps/backjune/src/main.ts
@@ -0,0 +1,60 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+import { Analytics } from '@june-so/analytics-node';
+import bodyParser from 'body-parser';
+
+const analytics = new Analytics('wpdpi6qyHu0VDkr7');
+const app = express();
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(bodyParser.json());
+
+/**
+ * @example
+ * http://localhost:3333/identify
+ * {
+ *   "id": "eaz56e4az6e4ae",
+ *   "email": "jk2@gmail.com",
+ *   "name": "Poi"
+ * }
+ */
+app.post('/identify', (req, res) => {
+  const { id, email, name } = req.body;
+  analytics.identify({
+    userId: id,
+    traits: {
+      email: email,
+      name: name,
+    },
+  });
+  res.send({ message: `User identified! ID: ${id} | name: ${name}` });
+});
+
+/**
+ * @example
+ * http://localhost:3333/track
+ * {
+ *   "id": "eaz56e4az6e4ae",
+ *   "whatever": "This is just a test"
+ * }
+ */
+app.post('/track', (req, res) => {
+  const { id } = req.body;
+  analytics.track({
+    userId: id,
+    event: 'TrackAPI',
+    properties: req.body,
+  });
+  res.send({ message: 'API tracked!' });
+});
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
